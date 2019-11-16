cca7a22807f1b3764fb86992e4ea6d8738cd389b
Montique Stevens <mstevens528@gmail.com>
2019-11-16 12:36:32 -0500

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -180,7 +180,7 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form method="post" action="montiquestevens@icloud.com">
+          <form method="post" action="mailto:montiquestevens@icloud.com">
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
